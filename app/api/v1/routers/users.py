6bfcc941f0dcf496a487786c6ac076f9eb95490b
OriShalhon <ori.shalhon@gmail.com>
2024-11-09 07:58:23 +0100

    minot change in launch.json

--- app/api/v1/routers/users.py
+++ app/api/v1/routers/users.py
@@ -14,7 +14,6 @@ router = APIRouter(
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    # hash the password - user.password
     hashed_password = utils.hash_password(user.password)
     user.password = hashed_password
 
