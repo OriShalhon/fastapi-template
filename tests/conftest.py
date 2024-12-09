0d41dfa0191f0429282f5b82587eda42e439d740
OriShalhon <ori.shalhon@gmail.com>
2024-12-09 19:43:13 +0100

    add basic tests for authorization

---
+++ tests/conftest.py
@@ -0,0 +1,50 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.core.settings import settings
+from app.db.database import Base, get_db
+from app.main import app
+
+TESTING_DATABASE_URL = settings.testing_database_url
+
+engine = create_engine(TESTING_DATABASE_URL)
+
+TestingSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture()
+def session():
+    print("my session fixture ran")
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSession()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture()
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "your_mail@gmail.com", "password": "password123"}
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+
+    new_user = res.json()
+    new_user["password"] = user_data["password"]
+    return new_user
